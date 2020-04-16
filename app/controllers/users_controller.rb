da926ba28639576082d068e056696488c8e6f04b
yuichi-kato <katochanpe.1005@softbank.ne.jp>
2020-04-16 11:07:02 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
-end
+
+  def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:succes] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  resources :users
 end
 
